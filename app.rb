ec97131ced97996b8702456f6083bc7d6923ebd9
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-11-21 16:58:19 -0500

    Add KENO games route

--- app.rb
+++ app.rb
@@ -3,8 +3,10 @@ require_relative 'lib/router'
 require_relative 'lib/web_service'
 
 get '/' do
-  router = MassLotteryAPI::Router.new
-  service = MassLotteryAPI::WebService.new(uri: router.keno_winning_numbers, params: [])
-  puts(service.fetch)
   erb(:index)
+end
+
+get '/keno/games.?:format?' do
+  content_type :json
+  erb('keno/games'.to_sym, layout: false)
 end
